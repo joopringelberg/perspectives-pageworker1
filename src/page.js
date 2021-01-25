c6af6300384ea687590677fa35734e87d954cfe2
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-25 15:59:49 +0100

    Initial commit for pageworker package.

---
+++ src/page.js
@@ -0,0 +1,186 @@
+// BEGIN LICENSE
+// Perspectives Distributed Runtime
+// Copyright (C) 2019 Joop Ringelberg (joopringelberg@perspect.it), Cor Baars
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Full text of this license can be found in the LICENSE file in the projects root.
+// END LICENSE
+
+////////////////////////////////////////////////////////////////////////////////
+//// PERSPECTIVES DISTRIBUTED RUNTIME
+////////////////////////////////////////////////////////////////////////////////
+let PDRPromise;
+
+////////////////////////////////////////////////////////////////////////////////
+//// INTERNAL CHANNEL
+////////////////////////////////////////////////////////////////////////////////
+import {InternalChannelPromise} from "perspectives-proxy";
+
+////////////////////////////////////////////////////////////////////////////////
+//// STORING PORTS SENT BY CLIENT PAGES
+////////////////////////////////////////////////////////////////////////////////
+const channels = {};
+let channelIndex = 1;
+
+////////////////////////////////////////////////////////////////////////////////
+//// PORT TO PAGE THAT HOSTS PDR
+//// RECEIVE PORTS FROM CLIENTS WHEN RUN IN THE MAIN PAGE, RELAYED THROUGH A SERVICE WORKER
+//// This function is passed on by the client in the call configurePDRProxy({pageHostingPDRPort: pageHostingPDRPort})
+////////////////////////////////////////////////////////////////////////////////
+function pageHostingPDRPort()
+{
+  // Create a channel.
+  const channel = new MessageChannel();
+  let weHost = false;
+
+  if ('serviceWorker' in navigator)
+  {
+    navigator.serviceWorker.register(
+      'perspectives-serviceWorker.js',
+      {
+          scope: './'
+      }).then(function (registration)
+        {
+          var serviceWorker;
+          if (registration.installing) {
+            serviceWorker = registration.installing;
+          } else if (registration.waiting) {
+            serviceWorker = registration.waiting;
+          } else if (registration.active) {
+            serviceWorker = registration.active;
+          }
+          if (serviceWorker)
+          {
+            // Send the port to the serviceWorker, to relay it to the page hosting the PDR.
+            navigator.serviceWorker.controller.postMessage({messageType: "relayPort", port: channel.port2});
+
+            // Listen to messages coming in from the serviceWorker.
+            navigator.serviceWorker.addEventListener('message', function(event)
+              {
+                switch (event.data.messageType){
+                  case "youHost":
+                    weHost = true;
+                    // This page must host the PDR.
+                    PDRPromise = import("perspectives-core");
+                    break;
+                  case "relayPort":
+                    // If we are the host, save the port; otherwise ignore.
+                    if (weHost)
+                    {
+                      // the new client (page) sends a port.
+                      channels[ channelIndex ] = event.data.port;
+                      // Return the channelIndex.
+                      channels[ channelIndex ].postMessage( {serviceWorkerMessage: "channelId", channelId: 1000000 * channelIndex });
+                      channelIndex = channelIndex + 1;
+                      // start listening to the new channel, handle requests.
+                      channels[ channelIndex ].onmessage = handleClientRequest;
+                    }
+                    break;
+                }
+              });
+          }
+          else
+          {
+            console.log ("Could not get serviceWorker from registration for an unknown reason.");
+          }
+        }).catch (function (error)
+          {
+            // Something went wrong during registration. The service-worker.js file
+            // might be unavailable or contain a syntax error.
+            console.log( error );
+          });
+  }
+  else
+  {
+      console.log( "This browser does not support service workers.");
+  }
+  // Use port1 in the SharedWorkerChannel.
+  return channel.port1;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//// HANDLE REQUESTS COMING IN THROUGH CHANNELS FROM CLIENTS
+////////////////////////////////////////////////////////////////////////////////
+// These calls are implemented in accordance with the types of the functions in the core.
+// The callbacks are declared as Effects, there, hence we treat them here that way.
+// We could cheat and provide callbacks that do not return an Effect.
+function handleClientRequest( request )
+{
+  const req = request.data;
+  if (req.proxyRequest)
+  {
+    switch (req.proxyRequest)
+    {
+      case "isUserLoggedIn":
+        //{proxyRequest: "isUserLoggedIn", channelId: proxy.channelId}
+        InternalChannelPromise.then( function ()
+          {
+            channels[corrId2ChannelId(req.channelId)].postMessage({serviceWorkerMessage: "isUserLoggedIn", isUserLoggedIn: true});
+          });
+        break;
+      case "authenticate":
+        PDRPromise.then( pdr => pdr.authenticate(req.username)(req.password)(req.host)(req.port)
+          (function(n) // (Int -> Effect Unit)
+            {
+              return function() //  This function is the result of the call to authenticate: the Effect.
+              {
+                // Find the channel.
+                channels[corrId2ChannelId(req.channelId)].postMessage({serviceWorkerMessage: "authenticate", authenticationResult: n});
+              };
+            })()); // The core authenticate function results in an Effect, hence we apply it to return the (integer) result.
+        break;
+      case "resetAccount":
+        PDRPromise.then( pdr.resetAccount(req.username)(req.password)(req.host)(req.port)
+          (function(success) // (Boolean -> Effect Unit)
+            {
+              return function() //  This function is the result of the call to resetAccount: the Effect.
+              {
+                channels[corrId2ChannelId(req.channelId)].postMessage({serviceWorkerMessage: "resetAccount", resetSuccesful: success });
+              };
+            })()); // The core authenticate function results in an Effect, hence we apply it to return the (boolean) result.
+
+        break;
+      case "close":
+        InternalChannelPromise.then( ic => ic.close() );
+        break;
+      case "unsubscribe":
+        InternalChannelPromise.then( ic => ic.unsubscribe( req.request ) );
+        break;
+    }
+  }
+  else
+  {
+    // The callback was saved in the ServiceWorkerChannel.
+    // Replace the callback with a function that passes on the response to the right channel.
+    // The ServiceWorkerChannel will apply the callback.
+    req.reactStateSetter = function( result )
+      {
+        return function()
+        {
+          channels[corrId2ChannelId(result.corrId)].postMessage( result );
+        };
+      };
+    InternalChannelPromise.then( ic => ic.send( req ) );
+  }
+}
+
+function corrId2ChannelId (corrId)
+{
+  return Math.floor(corrId / 1000000);
+}
+
+module.exports = {
+  pageHostingPDRPort: pageHostingPDRPort
+};
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    pageWorker: "./src/page.js"
+  },
+  output: {
+    library: 'perspectives-[name]',
+    filename: 'perspectives-[name].js',
+    path: path.join(__dirname, "dist"),
+    libraryTarget: "umd"
+  },
+  watch: false,
+  mode: "development",
+  target: "webworker",
+  module: {
+    rules: [{
+        test: /.js?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ]
+      }]
+  },
+  externals: {
+    // These are Affjax dependencies when running on node.
+    "xhr2-cookies": {
+      commonjs: "xhr2-cookies",
+      commonjs2: "xhr2-cookies",
+      amd: "xhr2-cookies",
+      root: "xhr2-cookies"
+    },
+    "url": {
+      commonjs: "url",
+      commonjs2: "url",
+      amd: "url",
+      root: "url"
+    },
+    "perspectives-core": {
+
+    }
+  }
+};
